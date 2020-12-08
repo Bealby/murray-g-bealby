e6ec7835d6fdddfd8412d69166914cdb7651bcf8
Bealby <mgbealby@gmail.com>
2020-12-08 19:08:43 +0000

    Config emailjs settings

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,16 +1,25 @@
+// Script for EmailJS API as well as alerts
+// Environ variable set up for EMAILJS_KEY
 function sendMail(contactForm) {
-    emailjs.send("gmail", "murray-bealby", {
+    emailjs.send("gmail", "murray_bealby", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "message": contactForm.projectsummary.value
+        "comment": contactForm.comment.value
     })
     .then(
         function(response) {
+            // Clears contact form after message sent
+            document.getElementById("myForm").reset();
+             // Message Alert for 'Message Sent'
+            swal("Message Sent", "success");
             console.log("SUCCESS", response);
         },
         function(error) {
+            // Message Alert for 'Message Fail'
+            swal("Message Fail", "Please check connection and try again", "error");
             console.log("FAILED", error);
         }
     );
+
     return false;  // To block from loading a new page
 }
