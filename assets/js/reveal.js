6247d46c35fda17f16405f0ab370641f0b9b0936
Bealby <mgbealby@gmail.com>
2020-12-12 21:20:46 +0000

    Added reveal.js. Added keyframes for intro mb affects

---
+++ assets/js/reveal.js
@@ -0,0 +1,9 @@
+function showAbout() {
+  document.getElementById("about-reveal").style.visibility = "visible";
+}
+setTimeout("showAbout()", 1000); // after 1 sec
+
+function showResume() {
+  document.getElementById("resume-reveal").style.visibility = "visible";
+}
+setTimeout("showResume()", 5000); // after 5 secs
