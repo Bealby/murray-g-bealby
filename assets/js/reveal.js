2da0d11899b4c34d5d080542ba92ac8e2ad05534
Bealby <mgbealby@gmail.com>
2020-12-14 08:11:26 +0000

    Adjusted javascript time set for link animation

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,30 +1,24 @@
-
-
-
-
-
-// Mobile
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
-setTimeout("showAbout()", 6000); // after 6 sec
+setTimeout("showAbout()", 7000); // after 7 sec
 
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
-setTimeout("showResume()", 7000); // after 7 secs
+setTimeout("showResume()", 8000); // after 8 secs
 
 function showInterests() {
   document.getElementById("interests-reveal").style.visibility = "visible";
 }
-setTimeout("showInterests()", 8000); // after 8 secs
+setTimeout("showInterests()", 9000); // after 9 secs
 
 function showContact() {
   document.getElementById("contact-reveal").style.visibility = "visible";
 }
-setTimeout("showContact()", 9000); // after 9 secs
+setTimeout("showContact()", 10000); // after 10 secs
 
 function showProjects() {
   document.getElementById("projects-reveal").style.visibility = "visible";
 }
-setTimeout("showProjects()", 10000); // after 10 secs
+setTimeout("showProjects()", 11000); // after 11 secs
