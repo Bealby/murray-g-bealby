8ff629d8faa181ae7086b5da25883e6c22e5b539
Bealby <mgbealby@gmail.com>
2020-12-14 06:43:30 +0000

    Set up typewriter/spin for mb and desktop

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,24 +1,30 @@
+
+
+
+
+
+// Mobile
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
-setTimeout("showAbout()", 6000); // after 5 sec
+setTimeout("showAbout()", 6000); // after 6 sec
 
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
-setTimeout("showResume()", 7000); // after 5 secs
+setTimeout("showResume()", 7000); // after 7 secs
 
 function showInterests() {
   document.getElementById("interests-reveal").style.visibility = "visible";
 }
-setTimeout("showInterests()", 8000); // after 5 secs
+setTimeout("showInterests()", 8000); // after 8 secs
 
 function showContact() {
   document.getElementById("contact-reveal").style.visibility = "visible";
 }
-setTimeout("showContact()", 9000); // after 5 secs
+setTimeout("showContact()", 9000); // after 9 secs
 
 function showProjects() {
   document.getElementById("projects-reveal").style.visibility = "visible";
 }
-setTimeout("showProjects()", 10000); // after 5 secs
+setTimeout("showProjects()", 10000); // after 10 secs
