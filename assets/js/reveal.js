0fe03557f1e85da74132fe6ba25995872e4031cf
Bealby <mgbealby@gmail.com>
2020-12-14 06:12:20 +0000

    Set up link reveal and spin in intro.hmtl mb

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,9 +1,24 @@
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
-setTimeout("showAbout()", 1000); // after 1 sec
+setTimeout("showAbout()", 6000); // after 5 sec
 
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
-setTimeout("showResume()", 5000); // after 5 secs
+setTimeout("showResume()", 7000); // after 5 secs
+
+function showInterests() {
+  document.getElementById("interests-reveal").style.visibility = "visible";
+}
+setTimeout("showInterests()", 8000); // after 5 secs
+
+function showContact() {
+  document.getElementById("contact-reveal").style.visibility = "visible";
+}
+setTimeout("showContact()", 9000); // after 5 secs
+
+function showProjects() {
+  document.getElementById("projects-reveal").style.visibility = "visible";
+}
+setTimeout("showProjects()", 10000); // after 5 secs
