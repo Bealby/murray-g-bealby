2077880ff5f7f2871205e00a2fb42349372b190a
Bealby <mgbealby@gmail.com>
2020-12-14 15:37:37 +0000

    RubberBand feature. Added javascript/css/html settings. Updated README

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 #### Full Screen Overlay Responsive Nav Bar
 https://github.com/Rustcodeweb/Full-Screen-Overlay-Responsive-Navigation-Bar
+
+#### RubberBand Feature
+http://www.theappguruz.com/tag-tools/web/CSSAnimations/
--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,24 +1,37 @@
+// Reveal settings for icons in index.html
+
+// ABOUT
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
 setTimeout("showAbout()", 6000); // after 6 sec
 
+// RESUME
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
 setTimeout("showResume()", 7000); // after 7 secs
 
+// INTERESTS
 function showInterests() {
   document.getElementById("interests-reveal").style.visibility = "visible";
 }
 setTimeout("showInterests()", 8000); // after 8 secs
 
+// CONTACT
 function showContact() {
   document.getElementById("contact-reveal").style.visibility = "visible";
 }
 setTimeout("showContact()", 9000); // after 9 secs
 
+// PROJECTS
 function showProjects() {
   document.getElementById("projects-reveal").style.visibility = "visible";
 }
 setTimeout("showProjects()", 10000); // after 10 secs
+
+// CV
+function showCv() {
+  document.getElementById("cv-reveal").style.visibility = "visible";
+}
+setTimeout("showCv()", 12000); // after 13 secs
