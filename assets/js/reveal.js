3cc53ccad2bb9a67a30473d925a089b14857b0ce
Bealby <mgbealby@gmail.com>
2020-12-14 08:25:31 +0000

    Adjusted timeset in styling and javascript

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,24 +1,24 @@
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
-setTimeout("showAbout()", 7000); // after 7 sec
+setTimeout("showAbout()", 6000); // after 6 sec
 
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
-setTimeout("showResume()", 8000); // after 8 secs
+setTimeout("showResume()", 7000); // after 7 secs
 
 function showInterests() {
   document.getElementById("interests-reveal").style.visibility = "visible";
 }
-setTimeout("showInterests()", 9000); // after 9 secs
+setTimeout("showInterests()", 8000); // after 8 secs
 
 function showContact() {
   document.getElementById("contact-reveal").style.visibility = "visible";
 }
-setTimeout("showContact()", 10000); // after 10 secs
+setTimeout("showContact()", 9000); // after 9 secs
 
 function showProjects() {
   document.getElementById("projects-reveal").style.visibility = "visible";
 }
-setTimeout("showProjects()", 11000); // after 11 secs
+setTimeout("showProjects()", 10000); // after 10 secs
