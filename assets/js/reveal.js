28a2d494bc2bcd9969a5dff022829f6f5f2bd12b
Bealby <mgbealby@gmail.com>
2021-02-24 16:03:51 +0000

    Faster animation for typewrite dt. Javascript/ css

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -4,34 +4,34 @@
 function showAbout() {
   document.getElementById("about-reveal").style.visibility = "visible";
 }
-setTimeout("showAbout()", 6000); // after 6 sec
+setTimeout("showAbout()", 4000); // after 4 sec
 
 // RESUME
 function showResume() {
   document.getElementById("resume-reveal").style.visibility = "visible";
 }
-setTimeout("showResume()", 7000); // after 7 secs
+setTimeout("showResume()", 5000); // after 5 secs
 
 // INTERESTS
 function showInterests() {
   document.getElementById("interests-reveal").style.visibility = "visible";
 }
-setTimeout("showInterests()", 8000); // after 8 secs
+setTimeout("showInterests()", 6000); // after 6 secs
 
 // CONTACT
 function showContact() {
   document.getElementById("contact-reveal").style.visibility = "visible";
 }
-setTimeout("showContact()", 9000); // after 9 secs
+setTimeout("showContact()", 7000); // after 7 secs
 
 // PROJECTS
 function showProjects() {
   document.getElementById("projects-reveal").style.visibility = "visible";
 }
-setTimeout("showProjects()", 10000); // after 10 secs
+setTimeout("showProjects()", 8000); // after 8 secs
 
 // CV
 function showCv() {
   document.getElementById("cv-reveal").style.visibility = "visible";
 }
-setTimeout("showCv()", 12000); // after 13 secs
+setTimeout("showCv()", 10000); // after 10 secs
